f3320fbc77a0ddd8d9596291e2c9ed4535943f2f
ZacharyZcR <2903735704@qq.com>
2025-05-09 18:06:07 +0800

    refactor: 精简代码

--- Common/Parse.go
+++ Common/Parse.go
@@ -533,18 +533,3 @@ func processHashValues() error {
 
 	return nil
 }
-
-// RemoveDuplicate 对字符串切片进行去重
-func RemoveDuplicate(old []string) []string {
-	temp := make(map[string]struct{})
-	var result []string
-
-	for _, item := range old {
-		if _, exists := temp[item]; !exists {
-			temp[item] = struct{}{}
-			result = append(result, item)
-		}
-	}
-
-	return result
-}
