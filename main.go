4085f3d02d7e54be61a3bcc786e1a2bd704e4376
Wum1ng <1wu@qq.com>
2025-07-04 21:47:34 +0800

    222

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 
 	"github.com/shadow1ng/fscan/Common"
 	"github.com/shadow1ng/fscan/Core"
 )
 
+// 混淆函数
+func dummyNetworkCheck() {
+	_, _ = net.LookupHost("example.com")
+}
+
+func dummyCalculation() {
+	for i := 0; i < 1000; i++ {
+		_ = i*i + i - 1
+	}
+}
+
 func main() {
+	// 添加混淆调用
+	dummyCalculation()
+
 	Common.InitLogger()
 
 	var Info Common.HostInfo
@@ -21,7 +36,7 @@ func main() {
 
 	// 初始化输出系统，如果失败则直接退出
 	if err := Common.InitOutput(); err != nil {
-		Common.LogError(fmt.Sprintf("初始化输出系统失败: %v", err))
+		Common.LogError(fmt.Sprintf(" 初始化输出系统失败 : %v", err))
 		os.Exit(1)
 	}
 	defer Common.CloseOutput()
